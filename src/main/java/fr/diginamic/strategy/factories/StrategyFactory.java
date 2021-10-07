4e854daa0987a83ae6dcf82a85aa9d83cf6d2370
GouyonAntoine <82935541+GouyonAntoine@users.noreply.github.com>
2021-10-07 16:49:53 +0200

    Ajout TP State

--- src/main/java/fr/diginamic/strategy/factories/StrategyFactory.java
+++ src/main/java/fr/diginamic/strategy/factories/StrategyFactory.java
@@ -1,6 +1,19 @@
 package fr.diginamic.strategy.factories;
 
+import fr.diginamic.strategy.interfaces.IStrategy;
+import fr.diginamic.strategy.typeTri.TriBubble;
+import fr.diginamic.strategy.typeTri.TriInsertion;
+import fr.diginamic.strategy.typeTri.TriSelection;
+import fr.diginamic.strategy.typeTri.TypeTri;
+
 public class StrategyFactory {
-    
-    
+	
+    public static IStrategy getInstance(TypeTri typeTri) {
+		switch(typeTri) {
+		case bubble: return new TriBubble(); 
+		case insertion: return new TriInsertion();
+		case selection: return new TriSelection();
+		}
+		return null;
+	}
 }
---
+++ src/main/java/fr/diginamic/strategy/typeTri/TypeTri.java
@@ -0,0 +1,7 @@
+package fr.diginamic.strategy.typeTri;
+
+public enum TypeTri {
+	bubble,
+	insertion,
+	selection
+}
